12a0014eaa7a9ff2ae97ce5a7b0ca3914a3bffe8
m4tewoosh <matnowak.web@gmail.com>
2021-06-20 13:26:22 +0200

    add home gradient name, header styling

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -6,7 +6,9 @@ export const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  padding: 7.5rem 23rem;
+  padding: 0 23rem;
+  /* padding-top: 7.5rem; */
+  height: 11rem;
   nav {
     display: flex;
     align-items: center;
@@ -28,26 +30,20 @@ export const StyledHeader = styled.header`
             color: #fffeff;
 
             &:after {
-              width: 100%;
-              left: 0;
+              width: 80%;
+              left: 10%;
             }
           }
 
           &:after {
             content: '';
             position: absolute;
-            display: block;
             width: 0;
             bottom: -10px;
             left: 50%;
             height: 4px;
             border-radius: 100px;
-            background: linear-gradient(
-                123.35deg,
-                #ebf3d0 0%,
-                rgba(235, 243, 208, 0) 18.4%
-              ),
-              radial-gradient(
+            background: radial-gradient(
                 29.9% 70.94% at 44.25% 86.96%,
                 #dc8ddc 0%,
                 rgba(220, 141, 220, 0) 100%
@@ -57,34 +53,11 @@ export const StyledHeader = styled.header`
                 #dc8ddc 0%,
                 rgba(220, 141, 220, 0) 100%
               ),
-              radial-gradient(
-                42.66% 49.72% at 45.56% 44.65%,
-                #cbadeb 0%,
-                rgba(194, 166, 241, 0) 100%
-              ),
-              linear-gradient(
-                134.59deg,
-                #cdf9e8 20.63%,
-                rgba(205, 249, 232, 0) 47.84%
-              ),
-              linear-gradient(
-                216.44deg,
-                rgba(192, 169, 240, 0) -16.52%,
-                #c0a9f0 -1.04%,
-                rgba(192, 169, 240, 0) 16.99%
-              ),
               linear-gradient(
                 128.53deg,
                 rgba(192, 169, 240, 0) 28.63%,
                 #c0a9f0 38.5%,
                 rgba(192, 169, 240, 0) 50.26%
-              ),
-              radial-gradient(
-                40.75% 97.37% at 90.75% 40.15%,
-                #fffdb1 0%,
-                #fee4bf 34.46%,
-                #f0bdd0 69.5%,
-                rgba(255, 129, 38, 0) 100%
               ),
               linear-gradient(0deg, #c2a6f1, #c2a6f1);
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,8 +7,10 @@ import { ReactComponent as CirclesSquare } from '../../assets/svg/circlesSquare.
 const StyledHome = styled.div`
   position: relative;
   width: 100vw;
+  /* height: calc(100vh - 8rem); */
   height: 100vh;
   background-color: #181333;
+  padding: 0 23rem;
 `;
 
 const StyledBlob = styled(Blob)`
@@ -19,12 +21,64 @@ const StyledBlob = styled(Blob)`
 
 const StyledCircle = styled(Circle)`
   position: absolute;
+  left: 0;
   bottom: 16.5rem;
 `;
 
 const StyledCirclesSquare = styled(CirclesSquare)`
   position: absolute;
-  bottom: 16.5rem;
+  left: -4rem;
+  top: -3rem;
+`;
+
+const StyledName = styled.h2`
+  position: relative;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 6.4rem;
+  font-weight: 700;
+  margin-top: 30rem;
+  color: white;
+
+  > span {
+    font-family: inherit;
+    font-weight: inherit;
+    font-size: inherit;
+    background-color: white;
+    background: linear-gradient(
+        123.35deg,
+        #ebf3d0 0%,
+        rgba(235, 243, 208, 0) 18.4%
+      ),
+      radial-gradient(
+        29.9% 70.94% at 44.25% 86.96%,
+        #dc8ddc 0%,
+        rgba(220, 141, 220, 0) 100%
+      ),
+      radial-gradient(
+        33.83% 53.57% at 35.87% 100%,
+        #dc8ddc 0%,
+        rgba(220, 141, 220, 0) 100%
+      ),
+      linear-gradient(
+        128.53deg,
+        rgba(192, 169, 240, 0) 28.63%,
+        #c0a9f0 38.5%,
+        rgba(192, 169, 240, 0) 50.26%
+      ),
+      linear-gradient(0deg, #c2a6f1, #c2a6f1);
+
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-text-fill-color: transparent;
+  }
+`;
+
+const StyledContent = styled.div`
+  width: 100%;
+  padding-top: 12rem;
 `;
 
 const Home: React.FC = () => {
@@ -32,7 +86,12 @@ const Home: React.FC = () => {
     <StyledHome>
       <StyledBlob />
       <StyledCircle />
-      {/* <StyledCirclesSquare /> */}
+      <StyledContent>
+        <StyledName>
+          Matthew <span>Nowak</span>
+          <StyledCirclesSquare />
+        </StyledName>
+      </StyledContent>
     </StyledHome>
   );
 };
