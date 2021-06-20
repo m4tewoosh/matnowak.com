40e0537c124329e81c2f1ee43e3b36551d881ee2
m4tewoosh <matnowak.web@gmail.com>
2021-06-20 16:37:10 +0200

    scrollbar color change

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -23,5 +23,30 @@ export default createGlobalStyle`
     /* } */
   }
 
+  ::-webkit-scrollbar-track {
+  /* box-shadow: inset 0 0 5px grey;  */
+  /* border-radius: 10px; */
+  /* background: transparent; */
+  /* background-color: red; */
+  /* display: none; */
+  background-color: transparent;
+}
+ 
+
+  ::-webkit-scrollbar {
+  width: 10px;
+  border-radius: 10px;
+
+}
+::-webkit-scrollbar-thumb {
+  background: linear-gradient(
+      128.53deg,
+      rgba(192, 169, 240, 0) 28.63%,
+      #c0a9f0 38.5%,
+      rgba(192, 169, 240, 0) 50.26%
+    ),
+    linear-gradient(0deg, #c2a6f1, #c2a6f1); 
+  border-radius: 10px;
+}
 
 `;
